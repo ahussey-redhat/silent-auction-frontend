9772a3519a55b31dc0412ecd986279e4d4cd7542
Alex Hussey <ahussey@redhat.com>
2024-07-24 06:12:36 +0000

    feat: Add combo LifeLine & Red Hat logo

--- src/components/Favicon/index.tsx
+++ src/components/Favicon/index.tsx
@@ -2,6 +2,6 @@ import { Helmet } from '@modern-js/runtime/head';
 
 export default () => (
   <Helmet>
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.svg" />
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.svg" />
   </Helmet>
 );
