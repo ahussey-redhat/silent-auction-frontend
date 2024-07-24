6e4324214678fd7a678fa1889233a88796c77499
Alex Hussey <ahussey@redhat.com>
2024-07-24 02:31:11 +0000

    fix: set MODERN_ENV for dev server

--- src/components/Favicon/index.tsx
+++ src/components/Favicon/index.tsx
@@ -2,10 +2,6 @@ import { Helmet } from '@modern-js/runtime/head';
 
 export default () => (
   <Helmet>
-    <link
-      rel="shortcut icon"
-      type="image/x-icon"
-      href="https://www.shareicon.net/download/2016/08/18/808754_hammer.svg"
-    />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.svg" />
   </Helmet>
 );
