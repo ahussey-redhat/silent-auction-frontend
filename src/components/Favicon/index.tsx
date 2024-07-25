f47ce7b827e9a4c32c1b8da8673d3f589bb1b2cd
Alex Hussey <ahussey@redhat.com>
2024-07-25 02:43:40 +0000

    fix: favicon href

--- src/components/Favicon/index.tsx
+++ src/components/Favicon/index.tsx
@@ -1,7 +1,8 @@
 import { Helmet } from '@modern-js/runtime/head';
+import imageSrc from './favicon.svg';
 
 export default () => (
   <Helmet>
-    <link rel="shortcut icon" type="image/x-icon" href="./favicon.svg" />
+    <link rel="shortcut icon" type="image/x-icon" href={imageSrc} />
   </Helmet>
 );
